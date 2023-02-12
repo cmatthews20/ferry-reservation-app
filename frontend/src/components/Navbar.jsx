00b33a560e16702a07374b2923ccf1d28f179ba3
cmatthews20 <colematthews717@gmail.com>
2023-02-11 20:59:37 -0500

    add navbar component and sample table for crossings

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,43 @@
+import {
+    ChakraProvider,
+    Button,
+    Grid,
+    GridItem,
+    Image,
+  } from "@chakra-ui/react";
+import Link from 'next/link';
+
+function Navbar() {
+    return(
+        <Grid
+        templateAreas={`"header""content"`}
+        templateRows="4rem 1fr"
+        templateColumns="1fr"
+      >
+        <GridItem
+          display="flex"
+          alignItems="center"
+          justifyContent="left"
+          area="header"
+          shadow="base"
+          bg="#D69E2E"
+          px={5}
+          py={4}
+        >
+          <Link href="/">
+            <Button colorScheme="">Crossings</Button>
+          </Link>
+          <Link href="/bookings">
+            <Button colorScheme="">My Bookings</Button>
+          </Link>
+          <Link href="/profile">
+            <Button colorScheme="">Profile</Button>
+          </Link>
+        </GridItem>
+        <GridItem area="content">
+        </GridItem>
+      </Grid>
+    );
+}
+
+export default Navbar;
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,9 +1,72 @@
-import {Flex} from "@chakra-ui/react";
+import { 
+  Wrap, 
+  Flex, 
+  Text, 
+  Image, 
+  Heading,
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer, 
+} from "@chakra-ui/react";
+import Navbar from '../components/Navbar.jsx';
 
 export default function Home() {
   return (
-    <Flex as="main" flexDir="column" px={20} alignItems="center">
-      Sample text, centred at the top of the page.
-    </Flex>
-  )
+    <>
+      <Navbar />
+      <Flex as="main" flexDir="column" px={20}>
+        <Flex
+          flexDir="column"
+          alignItems="center"
+          pt={16}
+          pb={10}
+          gap={4}
+          w="full"
+        >
+          <Heading as="h1" size="xl">
+            Choose a Crossing
+          </Heading>
+        </Flex>
+        <TableContainer>
+          <Table variant='simple'>
+            <TableCaption>Table Caption</TableCaption>
+            <Thead>
+              <Tr>
+                <Th>Crossing</Th>
+                <Th>Date</Th>
+                <Th>Departure</Th>
+                <Th>Seats Rem.</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td>Bell Island - Portugal Cove</Td>
+                <Td>2020/08/06</Td>
+                <Td>10:30am</Td>
+                <Td>60</Td>
+              </Tr>
+              <Tr>
+                <Td>Bell Island - Portugal Cove</Td>
+                <Td>2020/08/06</Td>
+                <Td>10:30am</Td>
+                <Td>60</Td>
+              </Tr>
+              <Tr>
+                <Td>Bell Island - Portugal Cove</Td>
+                <Td>2020/08/06</Td>
+                <Td>10:30am</Td>
+                <Td>60</Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </TableContainer>
+      </Flex>
+    </>
+  );
 }
