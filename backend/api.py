b2116f354190a3d3974f9251d4c993f0c9d7fa52
Mepha89 <umamarahman12@gmail.com>
2023-02-13 10:16:57 -0330

    UR-001: Updated comments and readme

--- backend/api.py
+++ backend/api.py
@@ -1,3 +1,10 @@
+'''
+This is the main API file for back-end endpoint management.
+FastAPI is a modern, high-performance web framework for building APIs with Python.
+We are using FastAPI to build the backend of our application to make calls to and from the database.
+Doing this allows us to read and write to the database and helps the front-end components use the data 
+in the SQLite database.
+'''
 from fastapi import FastAPI
 
 app = FastAPI()
