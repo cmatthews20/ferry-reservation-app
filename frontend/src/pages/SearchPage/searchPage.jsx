0d9abc6a008078cef5cb8691baa0263e1e6ea260
Shair Yousuf Jahin <71799869+ShairYousuf@users.noreply.github.com>
2023-02-20 16:37:56 -0330

    SYJ: 001 Added crossing page and date picker (#3)

---
+++ frontend/src/pages/SearchPage/searchPage.jsx
@@ -0,0 +1,17 @@
+//Creates the search page, components should include date picker, the output table and search bar
+
+import Head from 'next/head';
+import Navbar from '../../components/Navbar.jsx';
+import TableDatePicker from '../../components/datePicker.jsx';
+export default function searchPage() {
+    return(
+      <>
+        <Head>
+          <title>Book Crossings</title>
+        </Head>
+        <Navbar/>
+        <TableDatePicker/>
+        <h1>First Post</h1>
+      </>
+    );
+  }
