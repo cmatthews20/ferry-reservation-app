82bf3d774497f73e4cea5648e6694622e75c0c86
cmatthews20 <colematthews717@gmail.com>
2023-02-22 12:30:34 -0500

    database.py comment add

--- backend/database.py
+++ backend/database.py
@@ -1,3 +1,7 @@
+"""
+Initialize db location, engine, and base class.
+"""
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
