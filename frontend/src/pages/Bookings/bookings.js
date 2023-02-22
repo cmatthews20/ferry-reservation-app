6f2649b43df21ede9b0d41b1f191cf1842e828c7
Mepha89 <umamarahman12@gmail.com>
2023-02-22 19:32:37 -0330

    UR-005-Search-Bar-implementation-for-bookings

---
+++ frontend/src/pages/Bookings/bookings.js
@@ -0,0 +1,25 @@
+/*
+This page loads the Booking table component to display the booking searched by booking ID.
+*/
+import { Box, Center, Heading, VStack } from "@chakra-ui/react";
+import React from "react";
+import SearchBar from "../../components/SearchBar.jsx";
+import { DarkModeSwitch } from "../../components/DarkModeSwitch";
+import Navbar from '../../components/Navbar.jsx';
+
+export default function Bookings() {
+  return (
+    <>
+    <Navbar />
+      <Box h="100vh">
+        <DarkModeSwitch />
+          <VStack>
+            <Heading as='h2' size='2xl'>Bookings</Heading>
+                <Box>
+                   <SearchBar/>
+                </Box>
+          </VStack>
+      </Box>
+    </>
+  );
+}
