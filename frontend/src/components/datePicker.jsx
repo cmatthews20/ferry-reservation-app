c2767e6f2d8793fb0d55f6def48c9ca1abd95f97
ShairYousuf <syjahin@mun.ca>
2023-02-22 23:43:12 -0330

    Added fetch code, but error

--- frontend/src/components/datePicker.jsx
+++ frontend/src/components/datePicker.jsx
@@ -1,15 +1,55 @@
 //The date picker component fot the search bar
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import {
+  Box,
+  Button,
+  Flex,
+  Input,
+  InputGroup,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Stack,
+  Text,
+  useDisclosure
+} from "@chakra-ui/react";
 
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 export default function TableDatePicker() {
- const [startDate, setStartDate] = useState(new Date());
- const [endDate, setEndDate] = useState(new Date());
- 
+
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  async function fetchSchedule() {
+    try {
+        const response = await fetch('http://127.0.0.1:8000/schedules', {
+            method: 'GET',
+            headers: {
+                accept: 'application/json',
+            },
+        });
+        if (!response.ok) {
+            throw new Error(`Error! status: ${response.status}`);
+        }
+        const result = await response.json();
+        console.log(result);
+        return result;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+ useEffect(() => {
+  fetchSchedule();
+ }, []);
  return (
-   <div>
+
+    <div>
      <DatePicker
        selected={startDate}
        selectsStart
