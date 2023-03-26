27bf55e090d934c74eb5cd056cd05bb1db6da024
cmatthews20 <colematthews717@gmail.com>
2023-03-26 16:15:36 -0230

    landing page added

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,72 +1,21 @@
-import { 
-  Wrap, 
-  Flex, 
-  Text, 
-  Image, 
-  Heading,
-  Table,
-  Thead,
-  Tbody,
-  Tfoot,
-  Tr,
-  Th,
-  Td,
-  TableCaption,
-  TableContainer, 
-} from "@chakra-ui/react";
-import Navbar from '../components/Navbar.jsx';
+import Link from 'next/link';
+import { Button, AbsoluteCenter, Box } from '@chakra-ui/react'
 
-export default function Home() {
+function LandingPage() {
   return (
-    <>
-      <Navbar />
-      <Flex as="main" flexDir="column" px={20}>
-        <Flex
-          flexDir="column"
-          alignItems="center"
-          pt={16}
-          pb={10}
-          gap={4}
-          w="full"
-        >
-          <Heading as="h1" size="xl">
-            Choose a Crossing
-          </Heading>
-        </Flex>
-        <TableContainer>
-          <Table variant='simple'>
-            <TableCaption>Table Caption</TableCaption>
-            <Thead>
-              <Tr>
-                <Th>Crossing</Th>
-                <Th>Date</Th>
-                <Th>Departure</Th>
-                <Th>Seats Rem.</Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td>Bell Island - Portugal Cove</Td>
-                <Td>2020/08/06</Td>
-                <Td>10:30am</Td>
-                <Td>60</Td>
-              </Tr>
-              <Tr>
-                <Td>Bell Island - Portugal Cove</Td>
-                <Td>2020/08/06</Td>
-                <Td>10:30am</Td>
-                <Td>60</Td>
-              </Tr>
-              <Tr>
-                <Td>Bell Island - Portugal Cove</Td>
-                <Td>2020/08/06</Td>
-                <Td>10:30am</Td>
-                <Td>60</Td>
-              </Tr>
-            </Tbody>
-          </Table>
-        </TableContainer>
-      </Flex>
-    </>
+    <Box
+      backgroundImage="url('background.jpg')"
+      backgroundSize="cover"
+      backgroundPosition="center"
+      height="100vh"
+    >
+      <AbsoluteCenter>
+      <Link href="/SearchPage/searchPage">
+        <Button colorScheme='blue' size='lg'>Continue as Guest</Button>
+      </Link>
+      </AbsoluteCenter>
+      
+    </Box>
   );
 }
+export default LandingPage;
