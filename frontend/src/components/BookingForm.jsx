f933976f61eaa5852e558de717a72432223c9fdb
Mepha89 <umamarahman12@gmail.com>
2023-03-28 02:07:49 -0230

    UR-014-prettier-code

--- frontend/src/components/BookingForm.jsx
+++ frontend/src/components/BookingForm.jsx
@@ -14,27 +14,27 @@ import {
   SimpleGrid,
   GridItem,
   Checkbox,
-  Button,
-} from "@chakra-ui/react";
-import { PhoneIcon, EmailIcon, AddIcon } from "@chakra-ui/icons";
-import React, { useState } from "react";
-import { useRouter } from "next/router";
+  Button
+} from '@chakra-ui/react'
+import { PhoneIcon, EmailIcon, AddIcon } from '@chakra-ui/icons'
+import React, { useState } from 'react'
+import { useRouter } from 'next/router'
 
-const API_HOST = "http://127.0.0.1:8000";
-const CREATE_BOOKING_API_URL = `${API_HOST}/create_booking`;
+const API_HOST = 'http://127.0.0.1:8000'
+const CREATE_BOOKING_API_URL = `${API_HOST}/create_booking`
 
-function BookingForm({ schedule_data }) {
+function BookingForm ({ schedule_data }) {
   const [addFormData, setAddFormData] = useState({
-    name: "",
-    phone: "",
-    email: "",
-    additional_passengers: "",
-    vehicle: "",
-  });
+    name: '',
+    phone: '',
+    email: '',
+    additional_passengers: '',
+    vehicle: ''
+  })
 
-  const router = useRouter();
+  const router = useRouter()
 
-  async function createBooking(
+  async function createBooking (
     name,
     email,
     phone,
@@ -42,41 +42,41 @@ function BookingForm({ schedule_data }) {
     vehicle_id,
     additional_passengers
   ) {
-    const url = `${CREATE_BOOKING_API_URL}?name=${name}&email=${email}&phone=${phone}&schedule_id=${schedule_id}&vehicle_id=${vehicle_id}&additional_passengers=${additional_passengers}`;
+    const url = `${CREATE_BOOKING_API_URL}?name=${name}&email=${email}&phone=${phone}&schedule_id=${schedule_id}&vehicle_id=${vehicle_id}&additional_passengers=${additional_passengers}`
     const options = {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
-      },
-    };
-    const response = await fetch(url, options);
-    if (!response.ok) {
-      throw new Error(`Error! status: ${response.status}`);
+        'Content-Type': 'application/json'
+      }
     }
-    const data = await response.json();
-    return data;
+    const response = await fetch(url, options)
+    if (!response.ok) {
+      throw new Error(`Error! status: ${response.status}`)
+    }
+    const data = await response.json()
+    return data
   }
 
-  const handleAddFormChange = (event) => {
-    const fieldName = event.target.getAttribute("name");
-    const fieldValue = event.target.value;
+  const handleAddFormChange = event => {
+    const fieldName = event.target.getAttribute('name')
+    const fieldValue = event.target.value
 
-    const newFormData = { ...addFormData };
-    newFormData[fieldName] = fieldValue;
+    const newFormData = { ...addFormData }
+    newFormData[fieldName] = fieldValue
 
-    setAddFormData(newFormData);
-  };
+    setAddFormData(newFormData)
+  }
 
-  const handleAddFormSubmit = async (event) => {
-    event.preventDefault();
+  const handleAddFormSubmit = async event => {
+    event.preventDefault()
     const newBooking = {
       name: addFormData.name,
       phone: addFormData.phone,
       email: addFormData.email,
       additional_passengers: addFormData.additional_passengers,
-      vehicle: addFormData.vehicle,
-    };
-    console.log(newBooking);
+      vehicle: addFormData.vehicle
+    }
+    console.log(newBooking)
     try {
       const data = await createBooking(
         newBooking.name,
@@ -85,26 +85,26 @@ function BookingForm({ schedule_data }) {
         schedule_data.schedule_id,
         newBooking.vehicle,
         newBooking.additional_passengers
-      );
-      alert(`Booking created with ID: ${data.booking_id}`);
-      router.push("/../SearchPage/searchPage");
+      )
+      alert(`Booking created with ID: ${data.booking_id}`)
+      router.push('/../SearchPage/searchPage')
     } catch (error) {
-      console.error("Error:", error);
-      alert("An error occurred while creating the booking.");
+      console.error('Error:', error)
+      alert('An error occurred while creating the booking.')
     }
-  };
+  }
 
   return (
-    <VStack w="full" h="full" p={10} spacing={10} alignItems="flex-start">
-      <VStack spacing={3} alignItems="flex-start">
-        <Heading size="2xl">Your details</Heading>
+    <VStack w='full' h='full' p={10} spacing={10} alignItems='flex-start'>
+      <VStack spacing={3} alignItems='flex-start'>
+        <Heading size='xl'>Your details</Heading>
         <Text>Please fill out your reservation details.</Text>
       </VStack>
-      <SimpleGrid columns={2} columnGap={3} rowGap={7} w="full">
+      <SimpleGrid columns={2} columnGap={3} rowGap={7} w='full'>
         <GridItem colSpan={2}>
-          <Heading size="s">
-            {schedule_data.schedule_id} - {schedule_data.ferry_name} -{" "}
-            {schedule_data.depart_port} to {schedule_data.arrive_port} -{" "}
+          <Heading size='s'>
+            {schedule_data.schedule_id} - {schedule_data.ferry_name} -{' '}
+            {schedule_data.depart_port} to {schedule_data.arrive_port} -{' '}
             {schedule_data.time}
           </Heading>
         </GridItem>
@@ -112,8 +112,8 @@ function BookingForm({ schedule_data }) {
           <FormControl isRequired>
             <FormLabel>Full Name</FormLabel>
             <Input
-              name="name"
-              placeholder="John Doe"
+              name='name'
+              placeholder='John Doe'
               onChange={handleAddFormChange}
             />
           </FormControl>
@@ -123,13 +123,13 @@ function BookingForm({ schedule_data }) {
             <FormLabel>Phone</FormLabel>
             <InputGroup>
               <InputLeftElement
-                pointerEvents="none"
-                children={<PhoneIcon color="gray.300" />}
+                pointerEvents='none'
+                children={<PhoneIcon color='gray.300' />}
               />
               <Input
-                name="phone"
-                type="tel"
-                placeholder="Phone number"
+                name='phone'
+                type='tel'
+                placeholder='Phone number'
                 onChange={handleAddFormChange}
               />
             </InputGroup>
@@ -140,13 +140,13 @@ function BookingForm({ schedule_data }) {
             <FormLabel>Email</FormLabel>
             <InputGroup>
               <InputLeftElement
-                pointerEvents="none"
-                children={<EmailIcon color="gray.300" />}
+                pointerEvents='none'
+                children={<EmailIcon color='gray.300' />}
               />
               <Input
-                name="email"
-                type="email"
-                placeholder="Email"
+                name='email'
+                type='email'
+                placeholder='Email'
                 onChange={handleAddFormChange}
               />
             </InputGroup>
@@ -157,13 +157,13 @@ function BookingForm({ schedule_data }) {
             <FormLabel>Additional Passengers</FormLabel>
             <InputGroup>
               <InputLeftElement
-                pointerEvents="none"
-                children={<AddIcon color="gray.300" />}
+                pointerEvents='none'
+                children={<AddIcon color='gray.300' />}
               />
               <Input
-                name="additional_passengers"
-                type="number"
-                placeholder="0"
+                name='additional_passengers'
+                type='number'
+                placeholder='0'
                 onChange={handleAddFormChange}
               />
             </InputGroup>
@@ -171,15 +171,15 @@ function BookingForm({ schedule_data }) {
         </GridItem>
         <GridItem colSpan={1}>
           <FormLabel>Vehicle</FormLabel>
-          <Checkbox name="vehicle" value="Yes" onChange={handleAddFormChange}>
+          <Checkbox name='vehicle' value='Yes' onChange={handleAddFormChange}>
             Personal Vehicle
           </Checkbox>
         </GridItem>
         <GridItem colSpan={2}>
           <Button
-            size="lg"
-            w="full"
-            type="submit"
+            size='lg'
+            w='full'
+            type='submit'
             onClick={handleAddFormSubmit}
           >
             Place Reservation
@@ -187,7 +187,7 @@ function BookingForm({ schedule_data }) {
         </GridItem>
       </SimpleGrid>
     </VStack>
-  );
+  )
 }
 
-export default BookingForm;
+export default BookingForm
--- frontend/src/components/BookingsTable.jsx
+++ frontend/src/components/BookingsTable.jsx
@@ -2,7 +2,7 @@
 This is the bookings table component that uses the Bookings Search Bar component to display searched bookings
 */
 
-import React, { useState } from "react";
+import React, { useState } from 'react'
 import {
   Table,
   Thead,
@@ -10,88 +10,88 @@ import {
   Tr,
   Th,
   Td,
-  TableContainer,
-} from "@chakra-ui/react";
+  TableContainer
+} from '@chakra-ui/react'
 
-import BookingsSearchBar from "./BookingsSearchBar";
+import BookingsSearchBar from './BookingsSearchBar'
 
-const API_HOST = "http://127.0.0.1:8000";
-const BOOKING_API_URL = `${API_HOST}/booking_data`;
-const PORTS_API_URL = `${API_HOST}/ports`;
+const API_HOST = 'http://127.0.0.1:8000'
+const BOOKING_API_URL = `${API_HOST}/booking_data`
+const PORTS_API_URL = `${API_HOST}/ports`
 
-export default function BookingsTable() {
-  const [bookingData, setBookingData] = useState([]);
-  const [portsData, setPortsData] = useState([]);
-  var dataWithPorts = [];
+export default function BookingsTable () {
+  const [bookingData, setBookingData] = useState([])
+  const [portsData, setPortsData] = useState([])
+  var dataWithPorts = []
 
-  function handleSearch(booking_id) {
-    fetchBookingData(booking_id);
-    fetchPortsData();
+  function handleSearch (booking_id) {
+    fetchBookingData(booking_id)
+    fetchPortsData()
   }
 
-  async function fetchBookingData(BOOKING_ID) {
+  async function fetchBookingData (BOOKING_ID) {
     try {
       const response = await fetch(`${BOOKING_API_URL}/${BOOKING_ID}`, {
-        method: "GET",
+        method: 'GET',
         headers: {
-          accept: "application/json",
-        },
-      });
+          accept: 'application/json'
+        }
+      })
       if (!response.ok) {
-        throw new Error(`Error! status: ${response.status}`);
+        throw new Error(`Error! status: ${response.status}`)
       }
-      const result = await response.json();
-      setBookingData(result);
-      return result;
+      const result = await response.json()
+      setBookingData(result)
+      return result
     } catch (err) {
-      console.log(err);
+      console.log(err)
     }
   }
 
-  async function fetchPortsData() {
+  async function fetchPortsData () {
     try {
       const response = await fetch(`${PORTS_API_URL}`, {
-        method: "GET",
+        method: 'GET',
         headers: {
-          accept: "application/json",
-        },
-      });
+          accept: 'application/json'
+        }
+      })
       if (!response.ok) {
-        throw new Error(`Error! status: ${response.status}`);
+        throw new Error(`Error! status: ${response.status}`)
       }
-      const result = await response.json();
-      setPortsData(result);
-      return result;
+      const result = await response.json()
+      setPortsData(result)
+      return result
     } catch (err) {
-      console.log(err);
+      console.log(err)
     }
   }
 
   const getDataWithPorts = () => {
-    return bookingData.map((item) => {
+    return bookingData.map(item => {
       const arrivalPort = portsData.find(
-        (port) => port.port_id === item.Crossing.arrive_port
-      );
+        port => port.port_id === item.Crossing.arrive_port
+      )
       const departurePort = portsData.find(
-        (port) => port.port_id === item.Crossing.depart_port
-      );
+        port => port.port_id === item.Crossing.depart_port
+      )
       return {
         ...item,
         arrive_port_name: arrivalPort.port_name,
-        depart_port_name: departurePort.port_name,
-      };
-    });
-  };
+        depart_port_name: departurePort.port_name
+      }
+    })
+  }
 
   if (bookingData.length !== 0 && portsData.length !== 0) {
-    dataWithPorts = getDataWithPorts();
+    dataWithPorts = getDataWithPorts()
   }
 
   return (
     <>
       <BookingsSearchBar handleSearch={handleSearch} />
       <TableContainer>
-        <Table variant="simple" style={{ textAlign: "center" }}>
+        <Table variant='simple' style={{ textAlign: 'center' }}>
           <Thead>
             <Tr>
               <Th>Booking ID</Th>
@@ -105,7 +105,7 @@ export default function BookingsTable() {
             </Tr>
           </Thead>
           <Tbody>
-            {dataWithPorts.map((item) => (
+            {dataWithPorts.map(item => (
               <Tr>
                 <Td>{item.Booking.booking_id}</Td>
                 <Td>{item.User.name}</Td>
@@ -121,5 +121,5 @@ export default function BookingsTable() {
         </Table>
       </TableContainer>
     </>
-  );
+  )
 }
